e3d67f9a1288c354db1d81a57f70039056a1ff33
Thibaut792 <Thibaut.fiquet@ltpdampierre.fr>
2021-04-21 16:25:38 +0200

    Mise en place de bonus

--- core/src/fr/pgah/libgdx/Intro.java
+++ core/src/fr/pgah/libgdx/Intro.java
@@ -1,6 +1,7 @@
 package fr.pgah.libgdx;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -8,24 +9,26 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-
-
 public class Intro extends ApplicationAdapter {
 
-  final int NB_SPRITES = 5;    //nous déclarons une constante avec "final"
+  final int NB_SPRITES = 3; // nous déclarons une constante avec "final"
+  final int NB_BONUS = 1;
   SpriteBatch batch;
   int longueurFenetre;
   int hauteurFenetre;
   ArrayList<Sprite> sprites = new ArrayList<Sprite>(NB_SPRITES);
+  ArrayList<bonus> bonuss = new ArrayList<bonus>(NB_BONUS);
   Joueur joueur;
   Texture gameoverTexture;
   boolean gameOver;
   Souris souris;
+  bonus indexBonus;
   ArrayList<Sprite> spriteTouches = new ArrayList<>();
-  
+  Scanner lancerjeu = new Scanner(System.in);
+
   @Override
   public void create() {
-    
+
     batch = new SpriteBatch();
     gameOver = false;
     gameoverTexture = new Texture("gameover.jpg");
@@ -35,37 +38,58 @@ public class Intro extends ApplicationAdapter {
     initialisationSprites();
     initialiserJoueur();
     initialiserSouris();
-    
+    initialisationBonus();
+    lancergame();
+
+  }
+
+  private void lancergame() {
+
+    System.out.println("voulez vous lancer le jeu yes/no");
+    String a = lancerjeu.nextLine();
+    if (a.equals("yes")) {
+      gameOver = false;
+    } else {
+      gameOver = true;
+    }
+  }
+
+  private void initialisationBonus() {
+    for (int i = 0; i < NB_BONUS; i++) {
+      bonuss.add(new bonus("black.png"));
+    }
   }
 
   private void initialiserJoueur() {
     joueur = new Joueur();
-  } 
+  }
 
   private void initialiserSouris() {
-      souris = new Souris();
+    souris = new Souris();
   }
 
   private void initialisationSprites() {
-    //sprites = new Sprite[NB_SPRITES];
-    
-    for (int i = 0; i < NB_SPRITES; i++){
-      sprites.add( new Sprite("tortue.png")); // avec add pour fournir au sprite notre image
-      //sprites [i] = new Sprite("tortue.png");    // initialise un tableau pour chaque i 
-      // un constructeur = la fonction qui est appelé à la crétion de l'objet. Si il y a pas de constructeur java en initialise un tout seul par defaut.
-      //sprites[i].initialiser();      
+    // sprites = new Sprite[NB_SPRITES];
+
+    for (int i = 0; i < NB_SPRITES; i++) {
+      sprites.add(new Sprite("tortue.png")); // avec add pour fournir au sprite notre image
+      // sprites [i] = new Sprite("tortue.png"); // initialise un tableau pour chaque
+      // i
+      // un constructeur = la fonction qui est appelé à la crétion de l'objet. Si il y
+      // a pas de constructeur java en initialise un tout seul par defaut.
+      // sprites[i].initialiser();
     }
   }
 
- @Override
+  @Override
   public void render() {
 
-    if (!gameOver){
+    if (!gameOver) {
 
-    reinitialiserArrierePlan();
-    maJEtatProtagoniste();
-    majEtatjeux();
-    dessiner();
+      reinitialiserArrierePlan();
+      maJEtatProtagoniste();
+      majEtatjeux();
+      dessiner();
 
     }
   }
@@ -76,59 +100,74 @@ public class Intro extends ApplicationAdapter {
   }
 
   private void maJEtatProtagoniste() {
-    for(Sprite sprite:sprites) {
-    //for (int i = 0; i < sprites.length; i++) {
+    for (Sprite sprite : sprites) {
+      // for (int i = 0; i < sprites.length; i++) {
       sprite.maJEtat();
-    //sprites[i].maJEtat();
+      // sprites[i].maJEtat();
     }
+
+    for (bonus bonus : bonuss) {
+      bonus.majEtat();
+    }
+
     joueur.maJEtat();
     souris.maJEtat();
   }
 
   private void majEtatjeux() {
-    if(joueur.estEnCollisionAvec(sprites)) {
+    if (joueur.estEnCollisionAvec(sprites)) {
       gameOver = true;
     }
-    if(souris.clicGauchetir()){
+    if (souris.clicGauchetir()) {
 
     }
-    for (Sprite sprite:sprites){
-      if (souris.clicGauche() && sprite.seTrouveSur(souris) ) {
-        //spriteTouches = sprites;  // retenir le sprite dans une variable pour supprimé le sprite en dehors du for
-        spriteTouches.add(sprite);  // retenir le sprite dans une variable pour supprimé le sprite en dehors du for
+    for (Sprite sprite : sprites) {
+      if (souris.clicGauche() && sprite.seTrouveSur(souris)) {
+        // spriteTouches = sprites; // retenir le sprite dans une variable pour supprimé
+        // le sprite en dehors du for
+        spriteTouches.add(sprite); // retenir le sprite dans une variable pour
+        // supprimé le sprite en dehors du for
       }
     }
-      sprites.removeAll(spriteTouches); //supprimer tous les sprites qui se trouve sur le clique
-     // sprites.remove(spriteTouches); // supprimé le sprite en dehors du for car il ne faut surtout pas supprimé un élément dans un for 
-
-      if(sprites.isEmpty()){   // Quand il n'y a plus de sprite alors affiché une image ici gameover
-        gameOver = true;
-      }
-
-      if (souris.NBshoot == 0){
-        gameOver = true;
-        System.out.println("Vous n'avez plus de balles");
+    for (bonus bonus : bonuss) {
+      if (souris.clicGauche() && bonus.CollisionAvec(souris)) {
+        indexBonus = bonus;
+        souris.NBshoot += 3;
+        System.out.println("il vous reste " + souris.NBshoot + " balles");
       }
     }
-  
+    sprites.removeAll(spriteTouches); // supprimer tous les sprites qui se trouve sur le clique
+    bonuss.remove(indexBonus);
+    // ne faut surtout pas supprimé un élément dans un for
 
-    private void dessiner() {
-      batch.begin();
+    if (sprites.isEmpty()) { // Quand il n'y a plus de sprite alors affiché une image ici gameover
+      gameOver = true;
+    }
 
-    if(gameOver){
+    if (souris.NBshoot == 0) {
+      gameOver = true;
+      System.out.println("Vous n'avez plus de balles");
+    }
+  }
+
+  private void dessiner() {
+    batch.begin();
+
+    if (gameOver) {
       batch.draw(gameoverTexture, 50, 50);
-    }
-    else {
-      for(Sprite sprite:sprites) {
-      //for (int i = 0; i < sprites.length; i++) {
-      //sprites[i].dessiner(batch); // copîe la référence à l'objet batch 
-      sprite.dessiner(batch);
-    }
+    } else {
+      for (Sprite sprite : sprites) {
+        // for (int i = 0; i < sprites.length; i++) {
+        // sprites[i].dessiner(batch); // copîe la référence à l'objet batch
+        sprite.dessiner(batch);
+      }
+      for (bonus bonus : bonuss) {
+        bonus.dessiner(batch);
+      }
+      joueur.dessiner(batch);
+      souris.dessiner(batch);
 
-    joueur.dessiner(batch);
-    souris.dessiner(batch);
-    
     }
-  batch.end();
+    batch.end();
   }
 }
--- core/src/fr/pgah/libgdx/Souris.java
+++ core/src/fr/pgah/libgdx/Souris.java
@@ -9,127 +9,124 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class Souris {
 
-    
-    int coordX;
-    int coordY;
-    Texture img2;
-    int longueureffective;
-    int hauteureffective;
-    int longueurFenetre;
-    int hauteurFenetre;
-    Texture img;
-    final int vitesse = 5;
-    Rectangle rekt;
-    Boolean clickgauche;
-    int NBshoot = 2;
+  int coordX;
+  int coordY;
+  Texture img2;
+  int longueureffective;
+  int hauteureffective;
+  int longueurFenetre;
+  int hauteurFenetre;
+  Texture img;
+  final int vitesse = 5;
+  Rectangle rekt;
+  Boolean clickgauche;
+  int NBshoot = 10;
 
-   public Souris() {
-    
+  public Souris() {
+
     img2 = new Texture("viseur.png");
-    //coordX = 0;
-    //coordX = 0;
+    // coordX = 0;
+    // coordX = 0;
     longueureffective = img2.getWidth();
     hauteureffective = img2.getHeight();
     longueurFenetre = Gdx.graphics.getWidth();
     hauteurFenetre = Gdx.graphics.getHeight();
     rekt = new Rectangle(coordX, coordY, longueureffective, hauteureffective);
-    
-   }
 
-	public void maJEtat() {
-        Suivre_souris();
-        forcerAresterDansCadre();
-	}
-
-    private void Suivre_souris() {
-
-        coordX = Gdx.input.getX()-25;  // donne les emplacements en X et Y de la souris 
-        coordY = Gdx.graphics.getHeight() - Gdx.input.getY()-25;
-     }
-
-    private  void forcerAresterDansCadre() {
-         // Gestion bordure droite
-      if (coordX + longueureffective > longueurFenetre) {
-        coordX = longueurFenetre - longueureffective;
-      }
-
-      // Gestion bordure gauche
-      if (coordX < 0) {
-        coordX = 0;
-      }
-
-      // Gestion bordures haute
-      if (coordY + hauteureffective > hauteurFenetre) {
-        coordY = hauteurFenetre - hauteureffective;
-      }
-
-      // Gestion bordure basse
-      if (coordY < 0) {
-        coordY = 0;
-      }
-
-    rekt.setPosition(coordX, coordY); 
   }
-    
 
-  public void dessiner(SpriteBatch batch) {   //Les variables local sont supprimés dés que l'on sort de la méthode
-                                                // ici c'est la copie de la rérérence a l'objet batch qui disparait mais la réference continue a éxister dans la class principal 
-        batch.draw(img2,coordX, coordY);
-      }
+  public void maJEtat() {
+    Suivre_souris();
+    forcerAresterDansCadre();
+  }
 
-	public boolean estEnCollisionAvec(ArrayList<Sprite> sprites) {
+  private void Suivre_souris() {
 
-        // Pour chaque sprite dans srpites 
-        //si sprite touche le joueurs
-        // alors renvoyer vraie
-        // sinon faux 
-        // boucle for reach = Sprite = le type de tableaux, sprites c'est le nom du tableau que l'on va parcourrir, sprite est une variable
-        for (Sprite sprite : sprites){
-            if (estEnCollisionAvec(sprite)){
-               return true;
-              } 
-          }
-           return false;
-        }
-    private boolean estEnCollisionAvec(Sprite sprite){
-        if (rekt.overlaps(sprite.rekt)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public boolean clicGauche() {
-        if (entreeClicGauche()) {
-            return true;
-        }
-        return false;
+    coordX = Gdx.input.getX() - 25; // donne les emplacements en X et Y de la souris
+    coordY = Gdx.graphics.getHeight() - Gdx.input.getY() - 25;
+  }
+
+  private void forcerAresterDansCadre() {
+    // Gestion bordure droite
+    if (coordX + longueureffective > longueurFenetre) {
+      coordX = longueurFenetre - longueureffective;
     }
 
-    private boolean entreeClicGauche() {
-        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-            return true;
-        }
-        return false;
+    // Gestion bordure gauche
+    if (coordX < 0) {
+      coordX = 0;
     }
 
-    public boolean clicGauchetir(){
-      if (entrerclicGauchetir()){
-        NBshoot = NBshoot -1;
-        System.out.println("Il vous reste " + NBshoot + " balles");
+    // Gestion bordures haute
+    if (coordY + hauteureffective > hauteurFenetre) {
+      coordY = hauteurFenetre - hauteureffective;
+    }
+
+    // Gestion bordure basse
+    if (coordY < 0) {
+      coordY = 0;
+    }
+
+    rekt.setPosition(coordX, coordY);
+  }
+
+  public void dessiner(SpriteBatch batch) { // Les variables local sont supprimés dés que l'on sort de la méthode
+                                            // ici c'est la copie de la rérérence a l'objet batch qui disparait mais la
+                                            // réference continue a éxister dans la class principal
+    batch.draw(img2, coordX, coordY);
+  }
+
+  public boolean estEnCollisionAvec(ArrayList<Sprite> sprites) {
+
+    // Pour chaque sprite dans srpites
+    // si sprite touche le joueurs
+    // alors renvoyer vraie
+    // sinon faux
+    // boucle for reach = Sprite = le type de tableaux, sprites c'est le nom du
+    // tableau que l'on va parcourrir, sprite est une variable
+    for (Sprite sprite : sprites) {
+      if (estEnCollisionAvec(sprite)) {
         return true;
       }
-        return false;
     }
+    return false;
+  }
 
-    private boolean entrerclicGauchetir(){
-      if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-        return true;
+  private boolean estEnCollisionAvec(Sprite sprite) {
+    if (rekt.overlaps(sprite.rekt)) {
+      return true;
+    } else {
+      return false;
     }
-        return false;
+  }
+
+  public boolean clicGauche() {
+    if (entreeClicGauche()) {
+      return true;
     }
+    return false;
+  }
+
+  private boolean entreeClicGauche() {
+    if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean clicGauchetir() {
+    if (entrerclicGauchetir()) {
+      NBshoot = NBshoot - 1;
+      System.out.println("Il vous reste " + NBshoot + " balles");
+      return true;
+    }
+    return false;
+  }
+
+  private boolean entrerclicGauchetir() {
+    if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+      return true;
+    }
+    return false;
+  }
 }
-
-    
-
-
-
